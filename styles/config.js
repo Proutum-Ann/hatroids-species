12350e1e90b3dcccd023751b7ea9b99823eef38a
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2024-08-08 23:02:41 -0400

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "17gvdBvIbc_t9JUYGosGjjUaPY9KcUwYQBZ2s8wpVJY8";
 
 
 /* ------------------------------------------------------------------- */
